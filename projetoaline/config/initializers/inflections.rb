257d69f0078334adb7c1ad133114c955731fd79c
Frenetick <maiconpmoraes@hotmail.com>
2014-09-24 21:28:13 -0300

    a

--- projetoaline/config/initializers/inflections.rb
+++ projetoaline/config/initializers/inflections.rb
@@ -11,7 +11,12 @@
 # end
 
 # These inflection rules are supported but not enabled by default:
- ActiveSupport::Inflector.inflections(:en) do |inflect|
-   inflect.acronym 'RESTful'
-   inflect.irregular 'questao', 'questoes'
- end
+
+ActiveSupport::Inflector.inflections do |inflect|
+	inflect.irregular 'questao','questoes'
+end
+
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#  inflect.acronym 'RESTful'
+#   inflect.irregular 'questao', 'questoes'
+# end
