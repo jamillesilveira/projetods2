c4965a6a44a8154a04bc14a9ed287280710ee172
u <u@ta.(none)>
2014-09-26 00:18:44 -0300

    Inicializado o CRUD no aluno, feito new e index, falta consertar o delete e fazer o update #24

--- projetoaline/app/controllers/alunos_controller.rb
+++ projetoaline/app/controllers/alunos_controller.rb
@@ -1,6 +1,25 @@
 class AlunosController < ApplicationController
 
 	def index
-		@alunos = Aluno.all
+
+	end
+
+	def new
+		@aluno = Aluno.new
+	end
+
+	def create
+		@aluno = Aluno.new(params.require(:aluno).permit(:nome, :matricula, :dataDeNascimento, :email, :senha))
+		redirect_to :alunos
+	end
+
+	def show
+		
+	end
+
+	def destroy
+		a = Aluno.find(params[:id])
+		a.destroy
+		redirect_to :alunos, notice: "Seu cadastro foi excluído, #{a.nome}"
 	end
 end
