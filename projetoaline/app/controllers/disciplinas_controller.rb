26aa454b5e5e6b71891cddd0513beaec1390d67d
Frenetick <maiconpmoraes@hotmail.com>
2014-09-26 01:16:35 -0300

    Criei a view da criação de novas disciplinas. #33

--- projetoaline/app/controllers/disciplinas_controller.rb
+++ projetoaline/app/controllers/disciplinas_controller.rb
@@ -3,5 +3,9 @@ class DisciplinasController < ApplicationController
 		@disciplinas = Disciplina.all
 	end
 	def new
+		@disciplina = Disciplina.new
 	end
+	/def uptade
+		@disciplina = d.ip
+	end/
 end
