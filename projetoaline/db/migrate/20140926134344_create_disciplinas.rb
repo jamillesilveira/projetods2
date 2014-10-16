cbea5dfd41b80a56059661705eb10c3e56ec4bcf
Frenetick <maiconpmoraes@hotmail.com>
2014-10-16 17:43:31 -0300

    sl

--- projetoaline/db/migrate/20140926134344_create_disciplinas.rb
+++ projetoaline/db/migrate/20140926134344_create_disciplinas.rb
@@ -2,8 +2,7 @@ class CreateDisciplinas < ActiveRecord::Migration
   def change
     create_table :disciplinas do |t|
       t.string :descricaoDisciplina
-      t.integer :codigoDisciplina
-      t.references :aluno, index: true
+      t.string :codigoDisciplina
 
       t.timestamps
     end
