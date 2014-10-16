7e48b185c4c72052784f08820bab1eafcd987fbc
u <u@ta.(none)>
2014-10-16 15:34:05 -0300

    Alterações feitas para finalizar o CRUD do aluno #24

--- projetoaline/app/controllers/alunos_controller.rb
+++ projetoaline/app/controllers/alunos_controller.rb
@@ -3,11 +3,15 @@
 class AlunosController < ApplicationController
 
 	def index
-
+		@aluno = Aluno.all
 	end
 
 	def new
 		@aluno = Aluno.new
+	end
+
+	def edit
+		@aluno = Aluno.find(params[:id])
 	end
 
 	def create
@@ -16,8 +20,14 @@ class AlunosController < ApplicationController
 		redirect_to :alunos
 	end
 
+	def update
+		@aluno = Aluno.find(params[:id])
+		@aluno.update(params.require(:aluno).permit(:nome, :matricula, :dataDeNascimento, :email, :senha))
+		redirect_to :aluno
+	end
+
 	def show
-		@aluno = Aluno.all
+		@aluno = Aluno.find(params[:id])
 	end
 
 	def destroy
