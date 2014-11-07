6ac986fe8ff1d8a0d04426cb8de3e264f2a9e1e1
Frenetick <maiconpmoraes@hotmail.com>
2014-11-06 23:36:12 -0200

    Metodo login, de login_controller, definido. #60

--- projetoaline/app/controllers/login_controller.rb
+++ projetoaline/app/controllers/login_controller.rb
@@ -1,8 +1,18 @@
+# encoding: UTF-8
 class LoginController < ApplicationController
   def index
   end
 
   def login
+  	logado = Usuario.autenticar(params[:usuario],params[:senha])
+  	if logado.nil?
+  		flash[:error] = 'Dados incorretos'
+  		render :index
+
+  	else
+  		session[:sessao] = logado.nome
+  		redirect_to root_path, notice: 'Login efetuado.'
+  	end
   end
 
   def logout
